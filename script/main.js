db4b5da2f4b4559e4fd129e252935e3cb577934f
patnjacq <yan20004@byui.edu>
2022-01-15 10:58:07 -0800

    week 2

--- script/main.js
+++ script/main.js
@@ -1,12 +1,12 @@
 window.onload = function(){
     document.getElementById("byweek").innerHTML = createList();}
 
-const arr = ['<a href = "wdd330/week1/index.html">Week 1</a>', 'week 2', 'week 3'];
+const arr = ['<a href = "wdd330/week1/index.html">Week 1</a>', '<a href = "wdd330/week2/index.html">Week 2</a>', 'week 3'];
 
 function createList() { 
     let txt = '<ol>';
-    for (x in arr)
-    txt += '<li>'+ arr[x] + '</li>' ;
-    txt += '</ol>'
-return txt
+    for (x in arr){
+     txt += '<li>'+ arr[x] + '</li>' ;}
+     txt += '</ol>';
+return txt;
 }
