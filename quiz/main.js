948343863dd6f3f9fd1e1d66265a1d3ad34e6280
patnjacq <yan20004@byui.edu>
2022-01-22 14:17:02 -0800

    Week 3

---
+++ quiz/main.js
@@ -0,0 +1,6 @@
+const quiz = [
+    ["What is Superman's real name?","Clark Kent"],
+    ["What is Wonder Woman's real name?","Diana Prince"],
+    ["What is Batman's real name?","Bruce Wayne"]
+];
+alert('Welcome to Quiz Ninja!');
--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 window.onload = function(){
     document.getElementById("byweek").innerHTML = createList();}
 
-const arr = ['<a href = "wdd330/week1/index.html">Week 1</a>', '<a href = "wdd330/week2/index.html">Week 2</a>', 'week 3'];
+const arr = ['<a href = "wdd330/week1/index.html">Week 1</a>', '<a href = "wdd330/week2/index.html">Week 2</a>', '<a href = "wdd330/week3/index.html">Week 3</a>'];
 
 function createList() { 
     let txt = '<ol>';
