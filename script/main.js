6d37f1c7afd2672ce2a02a3c21220cdb382a884d
patnjacq <yan20004@byui.edu>
2022-01-29 18:29:44 -0800

    Week 4

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 window.onload = function(){
     document.getElementById("byweek").innerHTML = createList();}
 
-const arr = ['<a href = "wdd330/week1/index.html">Week 1</a>', '<a href = "wdd330/week2/index.html">Week 2</a>', '<a href = "wdd330/week3/index.html">Week 3</a>'];
+const arr = ['<a href = "wdd330/week1/index.html">Week 1</a>', '<a href = "wdd330/week2/index.html">Week 2</a>', '<a href = "wdd330/week3/index.html">Week 3</a>', '<a href = "wdd330/week4/index.html">Week 4</a>', '<a href = "wdd330/week5/index.html">Week 5</a>'];
 
 function createList() { 
     let txt = '<ol>';
