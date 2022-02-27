beb22da3ca2f4f7851f823a18c07ddeceac4d195
patnjacq <yan20004@byui.edu>
2022-02-26 20:56:27 -0800

    week 8

--- script/main.js
+++ script/main.js
@@ -2,7 +2,8 @@ window.onload = function(){
     document.getElementById("byweek").innerHTML = createList();}
 
 const arr = ['<a href = "wdd330/week1/index.html">Week 1</a>', '<a href = "wdd330/week2/index.html">Week 2</a>', '<a href = "wdd330/week3/index.html">Week 3</a>', '<a href = "wdd330/week4/index.html">Week 4</a>', '<a href = "wdd330/week5/index.html">Week 5</a>',
-'<a href = "wdd330/week6/todo-challenge/index.html">The Big Todo Challenge</a>', '<a href = "wdd330/week7/index.html">Week 7 Ajax and Functions</a>'];
+'<a href = "wdd330/week6/todo-challenge/index.html">The Big Todo Challenge</a>', '<a href = "wdd330/week7/index.html">Week 7 Ajax and Functions</a>', '<a href = "wdd330/week8/index.html">Week 8 Animations</a>', '<a href = "wdd330/week9/index.html">Week 9</a>', 
+'<a href = "wdd330/week10/index.html">Week 10</a>' ];
 
 function createList() { 
     let txt = '<ol>';
