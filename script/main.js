97122e0b8dfeab9e1128964b5f8db95906ab1854
patnjacq <yan20004@byui.edu>
2022-04-13 12:48:39 -0700

    add game to index

--- script/main.js
+++ script/main.js
@@ -3,7 +3,7 @@ window.onload = function(){
 
 const arr = ['<a href = "wdd330/week1/index.html">Week 1</a>', '<a href = "wdd330/week2/index.html">Week 2</a>', '<a href = "wdd330/week3/index.html">Week 3</a>', '<a href = "wdd330/week4/index.html">Week 4</a>', '<a href = "wdd330/week5/index.html">Week 5</a>',
 '<a href = "wdd330/week6/todo-challenge/index.html">The Big Todo Challenge</a>', '<a href = "wdd330/week7/index.html">Week 7 Ajax and Functions</a>', '<a href = "wdd330/week8/index1.html">Week 8 Animations</a>', '<a href = "wdd330/week9/index.html">Week 09 Windows Object and HTML5 API</a>', 
-'<a href = "wdd330/week10/index.html">Week 10 Fetch and Form Validation</a>', '<a href = "wdd330/week11/index.html">Project Update</a>','<a href = "wdd330/final/index.html">Final Project</a>' ];
+'<a href = "wdd330/week10/index.html">Week 10 Fetch and Form Validation</a>', '<a href = "wdd330/week11/index.html">Project Update</a>','<a href = "wdd330/week12/index.html">Updated WordUp</a>','<a href = "wdd330/final/index.html">Final Project</a>' ];
 
 function createList() { 
     let txt = '';
